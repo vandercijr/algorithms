de60fa844d6f91ba68a918e355d61c3a6f8179b3
vanderci curvelo junior <vandercijr@yahoo.com.br>
2020-05-12 19:54:25 -0300

    refactoring to iterative way instead of recursive

--- src/js/sort/sort.js
+++ src/js/sort/sort.js
@@ -1,25 +1,54 @@
 'use strict';
 
-const bubbleSort = (array_input) => {
+// recursive form
+// first implementation
+// const bubbleSort = (array_input) => {
+//   const asize = array_input.length;
+
+//   if (asize > 2) {
+//     let found = false;
+//     for (var i = 0; i < asize; i++) {
+//       if (array_input[i] > array_input[i+1]) {
+//         found = true;
+//         let auxiliar_data = array_input[i];
+
+//         array_input[i] = array_input[i+1];
+//         array_input[i+1] = auxiliar_data;
+//       }
+//     }
+
+//     if (!found) {
+//       return true;
+//     }
+
+//     bubbleSort(array_input);
+//   }
+// };
+
+//  the iterative way
+const bubbleSort = (array_input, order) => {
   const asize = array_input.length;
+  const swap = (swap_array, idxa, idxb) =>  {
+    let auxiliar = swap_array[idxa];
+
+    swap_array[idxa] = swap_array[idxb];
+    swap_array[idxb] = auxiliar;
+  }
 
   if (asize > 2) {
-    let found = false;
-    for (var i = 0; i < asize; i++) {
-      if (array_input[i] > array_input[i+1]) {
-        found = true;
-        let auxiliar_data = array_input[i];
+    let found = true;
 
-        array_input[i] = array_input[i+1];
-        array_input[i+1] = auxiliar_data;
+    while (found) {
+      found = false;
+
+      for (var i = 0; i < asize; i++) {
+        if (array_input[i] > array_input[i+1]) {
+          found = true;
+
+          swap(array_input, i, i+1);
+        }
       }
     }
-
-    if (!found) {
-      return true;
-    }
-
-    bubbleSort(array_input);
   }
 };
 
