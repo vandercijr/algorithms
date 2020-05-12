54aa172456d8fd757912025f8a2300c6eb0204f6
Vanderci <vandercijr@gmail.com>
2020-05-12 18:47:15 -0300

    applied linter fixes

--- src/js/sort/sort.js
+++ src/js/sort/sort.js
@@ -1,28 +1,28 @@
-'use strict'
+'use strict';
 
 const bubbleSort = (array_input) => {
-    const asize = array_input.length;
+  const asize = array_input.length;
 
-    if (asize > 2) {
-      let found = false;
-      for (var i = 0; i < asize; i++) {
-        if (array_input[i] > array_input[i+1]) {
-          found = true;
-          let auxiliar_data = array_input[i];
+  if (asize > 2) {
+    let found = false;
+    for (var i = 0; i < asize; i++) {
+      if (array_input[i] > array_input[i+1]) {
+        found = true;
+        let auxiliar_data = array_input[i];
 
-          array_input[i] = array_input[i+1];
-          array_input[i+1] = auxiliar_data;
-        }
+        array_input[i] = array_input[i+1];
+        array_input[i+1] = auxiliar_data;
       }
-
-      if (!found) {
-        return true;
-      }
-
-      bubbleSort(array_input);
     }
-}
+
+    if (!found) {
+      return true;
+    }
+
+    bubbleSort(array_input);
+  }
+};
 
 module.exports = {
   bubbleSort : bubbleSort
-}
+};
