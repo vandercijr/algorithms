71051374379023fcd2814b5aa5f4a1c6e1a79c91
Vanderci <vandercijr@gmail.com>
2020-05-13 20:42:09 -0300

    add addToTop method

--- src/js/list/linkedlist.js
+++ src/js/list/linkedlist.js
@@ -8,32 +8,65 @@
  *
  */
 
-const add = 
+const Node = {
+  object :  {},
+  next   :  {}
+};
 
-const linkedList = (array_input, order) => {
-  const asize = array_input.length;
+let head_node = {};
 
-  if (asize > 2) {
-    let found = true;
+const addToTop = (object)  =>  {
+  let next = {};
 
-    while (found) {
-      found = false;
-
-      for (var i = 0; i < asize; i++) {
-        let a = (order === 'desc') ? i+1 : i, 
-            b = (order === 'desc') ? i   : i+1;
-
-        let shouldSwap = array_input[a] > array_input[b];
-
-        if (shouldSwap) {
-          found = true;
-          swap(array_input, a, b);
-        }
-      }
-    }
+  if (head_node !== {}) {
+      next = head_node;
   }
+
+  head_node = Object.assign({}, Node);
+  head_node.object = object;
+  head_node.next   = next;
 };
 
-module.exports = {
-  bubbleSort : bubbleSort
+const remove = ()  =>  {
+
 };
+
+const find = ()  =>  {
+
+};
+
+const count = ()  =>  {
+
+};
+
+const list  = ()  =>  {
+
+}
+
+const linkedList = {
+  addToTop : addToTop
+}
+
+const s1 = {
+  name : 'vanderci',
+  age  : 38
+};
+
+const s2 = {
+  name : 'sarah',
+  age  : 34
+};
+
+const s3 = {
+  name : 'clara',
+  age  : 11
+};
+
+linkedList.addToTop(s1, {});
+
+linkedList.addToTop(s2, s1);
+
+linkedList.addToTop(s3, s2);
+
+console.log(head_node);
+console.log(head_node.next);
